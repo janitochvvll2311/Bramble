914630bcc18d3cb8f9acf986f15ef185d6c2ab17
CHAVA <janitochvvll2311@gmail.com>
2023-05-30 15:08:12 -0600

    layouts added

--- tst/bramble/viewbinding/main.js
+++
@@ -1,7 +0,0 @@
-import { getViews } from "../../../lib/bramble/viewbinding/viewbinding.js";
-
-let views = await getViews();
-console.log(views);
-
-views.title.innerText = "My Title";
-views.content.innerText = "My Content";
